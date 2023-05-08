9642bfc63e910300034bc68f43a5a92b4ccc9cbe
alereic01 <132936398+alereic01@users.noreply.github.com>
2023-05-08 20:29:25 +0300

    Add files via upload

---
+++ sketch_may8a.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <LedControl.h>
+#include <SoftwareSerial.h>
+
+int DIN = 11;
+int CS = 10;
+int  CLK = 9;
+SoftwareSerial sw (0, 1); // RX, TX
+LedControl lc=LedControl(DIN, CLK, CS,1);
+
+
+
+void setup()  {
+  Serial.begin(57600);
+  sw.begin(9600);
+  lc.shutdown(0,false);
+  lc.setIntensity(0,0);
+  lc.clearDisplay(0);
+}
+
+void  loop() {
+   char data = sw.read();
+   for(int i = 0;i<9;i++){
+  if(sw.available())
+  {
+      lc.setLed(0,i,data,true);
+    delay(1000);
+       lc.setLed(0,i,data,false);
+    }
+  }
+
+}
+/* for(int j=0;j<8;j++){
+     for(int i=0;i<8;i++){
+       lc.setLed(0,j,i,true);
+       delay(100);
+       lc.setLed(0,j,i,false);
+     }
+  }*/
